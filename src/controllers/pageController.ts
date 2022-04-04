c324eeab37a2220c14e3c2041c8a9e81081daf58
Hemesron Farias <centerofmails@gmail.com>
2022-04-04 15:52:28 -0300

    Implementando a ativacao do menu, de acordo com suas paginas

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,30 +1,43 @@
 import { Request, Response } from "express";
+import {createMenuObject} from '../helpers/createMenuObject'
 
 export const home = (req:Request, res: Response)=>{
-  const banner = {
-    title: 'Todos os animais',
-    background: 'allanimals.jpg'
-  }
-  res.render('pages/page', {banner})
+  
+  res.render('pages/page', {
+    menu: createMenuObject('all'),    
+    banner:{
+      title: 'Todos os animais',
+      background: 'allanimals.jpg'}
+    }
+  )
 }
 export const dogs = (req:Request, res: Response)=>{
-  const banner = {
-    title: 'Cachorros',
-    background: 'banner_dog.jpg'
-  }
-  res.render('pages/page', {banner})
+ 
+  res.render('pages/page', {
+    menu: createMenuObject('dog'),    
+    banner:{
+      title: 'Cachorros',
+      background: 'banner_dog.jpg'}
+    }
+  )
 }
 export const cats = (req:Request, res: Response)=>{
-  const banner = {
-    title: 'Gatos',
-    background: 'banner_cat.jpg'
-  }
-  res.render('pages/page', {banner})
+ 
+  res.render('pages/page', {
+    menu: createMenuObject('cat'),    
+    banner:{
+      title: 'Gatos',
+      background: 'banner_cat.jpg'}
+    }
+  )
 }
 export const fishes = (req:Request, res: Response)=>{
-  const banner = {
-    title: 'Peixes',
-    background: 'banner_fish.jpg'
-  }
-  res.render('pages/page', {banner})
+
+  res.render('pages/page', {
+    menu: createMenuObject('fish'),    
+    banner:{
+      title: 'Peixes',
+      background: 'banner_fish.jpg'}
+    }
+  )
 }
