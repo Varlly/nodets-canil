93618dc7d98e06e5e00720be2a38ec84f0e81067
Hemesron Farias <centerofmails@gmail.com>
2022-04-04 13:50:27 -0300

    Criando o html e a pasta partials

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -5,10 +5,13 @@ export const home = (req:Request, res: Response)=>{
 }
 export const dogs = (req:Request, res: Response)=>{
   //res.render('pages/page')
+  res.send('dogs')
 }
 export const cats = (req:Request, res: Response)=>{
   //res.render('pages/page')
+  res.send('cats')
 }
 export const fishes = (req:Request, res: Response)=>{
+  res.send('fishes')
   //res.render('pages/page')
 }
